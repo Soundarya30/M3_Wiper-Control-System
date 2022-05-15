4758b25d00e1392ae592fade302b95385611f4a4
Soundarya30 <101163038+Soundarya30@users.noreply.github.com>
2022-05-15 15:05:31 +0530

    Update Wiper_Control_System.c

--- 3_Implementation/src/Wiper_Control_System.c
+++ 3_Implementation/src/Wiper_Control_System.c
@@ -1,3 +1,4 @@
+#include"Wiper_Control_System.h"
 #include<stdio.h>
 #include <stdint.h>
 #include "stm32f4xx_hal.h"
@@ -43,7 +44,8 @@ int main(void)
   Delay(1250);
   HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_13);
   Delay(1250); 
-	}
+   }
+}
 }
 
 void Init_OnBoard_LEDs(void)
@@ -51,15 +53,15 @@ void Init_OnBoard_LEDs(void)
 	 __HAL_RCC_GPIOD_CLK_ENABLE();
 	GPIO_InitTypeDef BoardLEDs;
 	BoardLEDs.Mode = GPIO_MODE_OUTPUT_PP;
-	BoardLEDs.Pin = GPIO_PIN_14;
-  BoardLEDs.Pin = GPIO_PIN_15;
-  BoardLEDs.Pin = GPIO_PIN_12;
-  BoardLEDs.Pin = GPIO_PIN_13;
+        BoardLEDs.Pin = GPIO_PIN_14;
+        BoardLEDs.Pin = GPIO_PIN_15;
+        BoardLEDs.Pin = GPIO_PIN_12;
+        BoardLEDs.Pin = GPIO_PIN_13;
 	HAL_GPIO_Init(GPIOD, &BoardLEDs);
 }
-void Delay_s(volatile int time_s)
+void Delay(volatile int time)
 {
 	      int j;
-        for(j = 0; j < time_s*4000; j++)
+        for(j = 0; j < time*4000; j++)
             {}  
 }
